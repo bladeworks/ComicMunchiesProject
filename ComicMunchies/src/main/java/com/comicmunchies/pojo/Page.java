04404d8da06006690ef35664bf51d2364d938da1
blade <flyblade@gmail.com>
2013-10-19 16:42:00 -0700

    Add pojos. Add simple book list.

--- ComicMunchies/src/main/java/com/comicmunchies/pojo/Page.java
+++ ComicMunchies/src/main/java/com/comicmunchies/pojo/Page.java
@@ -4,8 +4,24 @@ package com.comicmunchies.pojo;
  * Created by blade on 10/19/13.
  */
 public class Page {
-
     private int no;
     private String path;
+
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 
 }
